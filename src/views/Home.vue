b751d075f37ac2e4c821015ea1a7ca7aa39ae652
jimmy <jonssonjimmy8@gmail.com>
2020-12-14 17:22:35 -0500

    Vue product app, display products array with created function on fron-end vue file.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    {{ products[7]}}
+    <div v-for="product in products">
+      <img v-bind:src="product.image_url" v-bind:alt="product.name">
+    <h2>{{ product.name }} </h2>
+    </div>
   </div>
 </template>
-
+<style>
+</style>
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      products: [],
+    };
+  },
+  created: function () {
+    console.log("print");
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      console.log("print print");
+      axios.get("/api/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
+  },
+};
 </script>
